9d11cfbae2995b91af2d6c2096428b4d6addab66
gaijianqiao <gaijianqiao@126.com>
2019-03-16 10:59:50 +0800

    add some swagger configuration

--- src/main/java/my/learn/boot/SpringBootMain.java
+++ src/main/java/my/learn/boot/SpringBootMain.java
@@ -2,8 +2,10 @@ package my.learn.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootMain {
     public static void main(String args[])
     {
--- src/main/java/my/learn/boot/controller/BootController.java
+++ src/main/java/my/learn/boot/controller/BootController.java
@@ -1,16 +1,29 @@
 package my.learn.boot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Api(value = "my first swagger program",tags = {"my swagger test"})
 @Controller
 public class BootController {
+    @ApiOperation("one get request")
     @ResponseBody
-    @RequestMapping("/hello")
-    public String helle()
+    @RequestMapping(value = "/hello",method = RequestMethod.GET )
+    public String hello()
     {
         return "hello boot";
+    }
+
+    @ApiOperation("a post request, it will say hell to the name you sended.")
+    @ResponseBody
+    @RequestMapping(value = "/sayhello",method = RequestMethod.POST )
+    public String say(@RequestParam(value = "name",required = true) String myName)
+    {
+        return "say "+myName+" hello";
     }
 
 }
